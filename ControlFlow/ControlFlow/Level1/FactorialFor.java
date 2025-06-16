a52a9da766b87496b24364994440d76de47dd845
teena gautam <teena3028gautam@gmail.com>
2025-06-16 22:04:56 +0530

    [Teena Gautam] Add. All Practice Problem for Day 3

---
+++ ControlFlow/ControlFlow/Level1/FactorialFor.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FactorialFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int n = sc.nextInt();
+
+        if (n < 0) {
+            System.out.println("Invalid input. Please enter a positive integer.");
+            return;
+        }
+
+        long factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+
+        System.out.println("Factorial of " + n + " is " + factorial);
+    }
+}
+
